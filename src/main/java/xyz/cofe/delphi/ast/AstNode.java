ee86fd06966de1d384fd65b8f5a5082130b9990e
Georgiy Kamnev <nt.gocha@gmail.com>
2023-10-13 01:32:23 +0500

    comments

--- src/main/java/xyz/cofe/delphi/ast/AstNode.java
+++ src/main/java/xyz/cofe/delphi/ast/AstNode.java
@@ -7,10 +7,19 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Абстрактный узел, нужен для доступа к дочерним элементам
+ */
 public interface AstNode
 {
     interface UpdateContext {}
 
+    /**
+     * Обновление поддерева узлов
+     * @param ctx контекст обновления
+     * @return новое поддерево, должно быть того же типа что реализует AstNode
+     * @see AstUpdate
+     */
     default Object astUpdate(AstUpdate.UpdateContext ctx) {
         return this;
     }
@@ -35,10 +44,18 @@ public interface AstNode
         return ImListLinked.of(node);
     }
 
+    /**
+     * Должен быть реализован в дочерних классах, для доступа к дочерним узлам
+     * @return дочерние узлы
+     */
     default ImList<? extends AstNode,?> nestedAstNodes() {
         return ImListLinked.of();
     }
 
+    /**
+     * Итератор по дочерним узлам, включая вложенные
+     * @return итератор, где элемент - путь к узлу, в пути последовательность элементов обычная (0 - корень)
+     */
     default Iterable<ImList<AstNode,?>> tree(){
         return () -> new AstTreeIterator(this);
     }
